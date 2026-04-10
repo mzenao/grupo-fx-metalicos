d8b979c07d62079470536d730574a2591e83b163
Mzenao <mzenao@hotmail.com>
2026-04-10 01:26:55 -0300

    Ajustes finais para primeiro deploy

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -7,6 +7,14 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  preview: {
+    allowedHosts: [
+      "app.grupofxmetalicos.com.br",
+      "www.app.grupofxmetalicos.com.br",
+      "grupofxmetalicos.com.br",
+      "www.grupofxmetalicos.com.br",
+    ],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
